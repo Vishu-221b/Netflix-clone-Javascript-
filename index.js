354bd7ef01bae1564266378a2f607946c2706d80
Vishu <Vishwajeetsinghchauhan63@gmail.com>
2023-05-05 17:35:48 +0530

    nav scrolling soved.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const header = document.querySelector('header');
+document.addEventListener('scroll',()=>{
+    const header = document.querySelector('header');
+    if (window.scrollY > 0) {
+        header.classList.add('fixed');
+      } else {
+        header.classList.remove('fixed');
+      }
 
-window.addEventListener('scroll', () => {
-  if (window.scrollY > 0) {
-    header.classList.add('sticky');
-  } else {
-    header.classList.remove('sticky');
-  }
-});
+})
